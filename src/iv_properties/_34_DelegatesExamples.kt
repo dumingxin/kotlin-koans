3b78e16561c27800aa6b8af6918055aba8778fd1
dmx <mail@dmx.name>
2017-06-28 23:21:55 +0800

    完成前35个练习

--- src/iv_properties/_34_DelegatesExamples.kt
+++ src/iv_properties/_34_DelegatesExamples.kt
@@ -2,9 +2,10 @@ package iv_properties
 
 import util.TODO
 import util.doc34
+import kotlin.properties.Delegates
 
 class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by todoTask34()
+    val lazyValue: Int by Delegate(initializer)
 }
 
 fun todoTask34(): Lazy<Int> = TODO(
@@ -14,3 +15,21 @@ fun todoTask34(): Lazy<Int> = TODO(
     """,
     documentation = doc34()
 )
+
+class Delegate(val initializer: () -> Int):Lazy<Int>{
+    private var isInit=false
+    private var privateValue=0
+    override val value: Int
+        get() {
+            if (!isInit){
+                isInit=true
+                privateValue=initializer()
+                return privateValue
+            }
+            return privateValue
+        }
+    override fun isInitialized(): Boolean {
+        return isInit
+    }
+
+}
